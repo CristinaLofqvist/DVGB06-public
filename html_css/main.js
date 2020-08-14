968398d2245067f043873ad5a05acf9700d35173
stina-Lo <root@kings-MacBook-Pro.local>
2020-08-14 19:43:43 +0200

    Clean up

--- html_css/main.js
+++
@@ -1,8 +0,0 @@
-function toggleHidden(id) {
-    var x = document.getElementById(id);
-    if (x.className === "hidden") {
-        x.className = "show";
-    } else {
-        x.className = "hidden";
-    }
-}
